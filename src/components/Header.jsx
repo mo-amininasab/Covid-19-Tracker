825dcc1be8cbb173e800967333ba38e50c016715
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-12 20:31:49 +0430

    Header added.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-
-import classes from "./Header.module.css";
-
 function Header() {
   const [selectedCountry, setSelectedCountry] = useState("global");
 
@@ -14,7 +11,23 @@ function Header() {
   };
 
   return (
-    <div className='text-red-500'>Hello</div>
+    <div className="flex font-noto p-2 m-4 border border-black justify-between items-center">
+      <h1 className="text-xl sm:text-3xl font-semibold ">Covid-19 Tracker</h1>
+      <select
+        className="px-3 py-1 w-20 sm:w-28 rounded-md"
+        value={selectedCountry}
+        onChange={handleChange}
+      >
+        <option value="global">Global</option>
+        {fetchedCountries.map((country, index) => (
+          <option key={index} value={country.countryInfo.iso3}>
+            {country.country}
+          </option>
+        ))}
+        <option value="">Helo</option>
+        <option value="">Helo</option>
+      </select>
+    </div>
   );
 }
 
