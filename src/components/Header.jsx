6d410a55b7cc2ef5c5db9688a2e044f813175c34
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-12 19:45:13 +0430

    material ui removed.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-import Box from "@material-ui/core/Box";
-import "./Header.module.css";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
 
 import classes from "./Header.module.css";
 
@@ -20,28 +14,7 @@ function Header() {
   };
 
   return (
-    <Box className={classes.header}>
-      <h1 className={classes['form-control__title']}>COVID-19 TRACKER</h1>
-      <FormControl variant="outlined" className={classes["form-control"]}>
-        <InputLabel id="demo-simple-select-outlined-label">Area</InputLabel>
-        <Select
-          labelId="demo-simple-select-outlined-label"
-          id="demo-simple-select-outlined"
-          value={selectedCountry}
-          onChange={handleChange}
-          label="Area"
-        >
-          <MenuItem key={0} value="global">
-            global
-          </MenuItem>
-          {fetchedCountries.map((country, index) => (
-            <MenuItem key={index} value={country.countryInfo.iso3}>
-              {country.country}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </Box>
+    <div className='text-red-500'>Hello</div>
   );
 }
 
