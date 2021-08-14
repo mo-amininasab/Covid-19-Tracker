bdfda8032982c6256fed8b6a27d287f37b71f4d4
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-08-14 13:58:57 +0430

    project cleaned up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { useEffect, useState } from "react";
-import ReactTooltip from "react-tooltip";
-
+import React, { useEffect } from "react";
 
 // api
-import {
-  fetchCountries,
-  fetchSummaryData,
-  fetchGlobalHistoricalData,
-} from "./components/api/index";
+import { fetchCountries, fetchSummaryData } from "./components/api/index";
 
 // redux
 import { countriesActions } from "./store/countriesSlice";
 import { summaryDataActions } from "./store/summarySlice";
-import { historicalDataActions } from "./store/historicalDataSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 // components
 import Header from "./components/Header";
@@ -23,17 +16,7 @@ import Map from "./components/Map";
 import Chart from "./components/Chart";
 
 function App() {
-  // const [tooltipContent, setTooltipContent] = useState("");
-  const reactTooltipContent = useSelector(state => state.reactTooltip.content);
-  
   const dispatch = useDispatch();
-  const fetchedCountries = useSelector((state) => state.countries.countries);
-  const fetchedSummaryData = useSelector(
-    (state) => state.summaryData.summaryData
-  );
-  const fetchedHistoricalData = useSelector(
-    (state) => state.historicalData.global
-  );
 
   useEffect(() => {
     const fetchCountriesFromAPI = async () => {
@@ -46,21 +29,9 @@ function App() {
       dispatch(summaryDataActions.setSummaryData(data));
     };
 
-    // const fetchHistoricalDataFromAPI = async () => {
-    //   const data = await fetchGlobalHistoricalData();
-    //   dispatch(historicalDataActions.setHistoricalGlobalData(data));
-    // };
-
     fetchCountriesFromAPI();
     fetchSummaryDataFromAPI();
-    // fetchHistoricalDataFromAPI();
   }, []);
-
-  
-  // console.log(fetchedCountries);
-  // console.log(fetchedSummaryData);
-  
-  // console.log(fetchedHistoricalData);
 
   return (
     <div className="flex flex-col lg:flex-row min-h-screen">
